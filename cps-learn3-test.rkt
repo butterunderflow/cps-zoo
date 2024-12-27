601d38576c4e8a90183b79a755d040f9f80ae8f3
butterunderflow <azhong.934@gmail.com>
2024-12-28 02:15:12 +0800

    copy learn2

---
+++ cps-learn3-test.rkt
@@ -0,0 +1,42 @@
+#lang racket
+(require "cps-learn3.rkt")
+(require test-engine/racket-tests)
+(check-expect (eval-top #t) #t)
+
+(check-random (eval-top (random 100)) (random 100))
+
+(check-expect (eval-top '((lambda x x) 1)) 1)
+
+(check-expect (eval-top '((lambda x (+ x 3)) 1)) 4)
+
+(check-expect (eval-top '(if (= 1 2) 7 3)) 3)
+
+(check-expect
+ (eval-top '(+ (call/cc (lambda return (+ 9999 (return (* 3 3))))) (* 4 4)))
+ 25)
+
+(check-expect (with-output-to-string
+                (lambda ()
+                  (eval-top '((lambda x (show x)) 10))))
+              "10\n")
+
+;; redirect eval's output to a string
+(define (eval-top-wrapped exp)
+  (with-output-to-string
+    (lambda ()
+      (eval-top exp))))
+
+(check-expect
+ (eval-top-wrapped
+  '(begin
+     (show 11)
+     (show 12)))
+ "11\n12\n")
+
+(check-expect
+ (eval-top
+  '(begin
+     1))
+ 1)
+(test)
+
---
+++ cps-learn3.rkt
@@ -0,0 +1,85 @@
+#lang racket
+
+(define (get x env)
+  (let ((value (assoc x env)))
+    (if value
+        (cdr value)
+        (error (string-append "variable " (symbol->string x) " not found")))))
+
+(define (operator? op)
+  (member op '(+ - * / =)))
+
+(define (eval-op op x y)
+  (match op
+    ['+ (+ x y)]
+    ['- (- x y)]
+    ['* (* x y)]
+    ['/ (/ x y)]
+    ['= (= x y)]))
+
+(define (eval-list eval-f lst env cont)
+  (match lst
+    ['() (cont void)]
+    [`(,ex) (eval-f ex env cont)]
+    [`(,head ,@res)
+     (let ((new-cont
+            (lambda (v)
+              (eval-list eval-f res env cont))))
+       (eval-f head env new-cont))]))
+
+(define (eval3 ex env cont)
+  ;; cps interpreter with call/cc operator
+  ;; call/cc stands for call with current continuation,
+  ;; when call a function with current continuation, interpreter will pass the continuation
+  ;; as argument to the function, and when call this continuation inside the function, 
+  ;; the control will enter this continuation and never come back again. 
+  (match ex
+    [b #:when (boolean? b) (cont b)]
+    [n #:when (number? n) (cont n)]
+    [x #:when (symbol? x) (cont (get x env))]
+    [`(lambda ,sym ,body)
+     (cont (lambda (x k)
+             (eval3 body (cons (cons sym x) env) k)))]
+    [`(,op ,arg0 ,arg1) #:when (operator? op)
+                        (let ((new-cont0
+                               (lambda (v0)
+                                 (let ((new-cont1
+                                        (lambda (v1)
+                                          (cont (eval-op op v0 v1)))))
+                                   (eval3 arg1 env new-cont1)))))
+                          (eval3 arg0 env new-cont0))]
+    [`(if ,ex0 ,ex1 ,ex2)
+     (let ((new-cont (lambda (v)
+                       (if v
+                           (eval3 ex1 env cont)
+                           (eval3 ex2 env cont)))))
+     (eval3 ex0 env new-cont))]
+    [`(begin ,@exs1)
+     (eval-list eval3 exs1 env cont)]
+    [`(call/cc ,ex)
+     (let ((new-cont (lambda (f)
+                       (f
+                        (lambda (v _k)
+                          ;; _k is ignored, when call this continuation, the control will never come back to caller
+                          (cont v))
+                        init-cont))))
+       (eval3 ex env new-cont))]
+    [`(,op ,arg)
+     (let ((new-cont0
+            (lambda (v0)
+              (let ((new-cont1
+                     (lambda (v1)
+                       (v0 v1 cont))))
+                (eval3 arg env new-cont1)))))
+       (eval3 op env new-cont0))]))
+
+(define default-env
+  (list `(show . ,(lambda (x cont) (cont (println x))))))
+
+(define init-cont
+  (lambda (x) x))
+
+(define (eval-top ex)
+  (eval3 ex default-env init-cont))
+
+(provide eval-top)
